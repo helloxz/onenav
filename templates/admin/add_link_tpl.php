1067002a84eea9402ab9142b9d693b53b3be3e24
xiaoz <xiaoz93@outlook.com>
2021-04-06 18:37:19 +0800

    add_link_tpl

---
+++ templates/admin/add_link_tpl.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+  <title>OneNav后台管理</title>
+  <link rel='stylesheet' href='https://libs.xiaoz.top/layui/v2.5.4/css/layui.css'>
+  <link rel='stylesheet' href='templates/admin/static/style.css'>
+</head>
+<body>
+<div>
+<!-- 内容主体区域 -->
+<div class="layui-row" style = "margin-top:18px;">
+	<div class="layui-container">
+    <div class="layui-col-lg6 layui-col-md-offset3">
+    <form class="layui-form">
+    <div class="layui-form-item">
+    <label class="layui-form-label">URL</label>
+    <div class="layui-input-block">
+      <input type="url" id = "url" name="url" required  lay-verify="required|url" placeholder="请输入有效链接" autocomplete="off" class="layui-input">
+    </div>
+  </div>
+  <div class="layui-form-item">
+    <label class="layui-form-label">链接名称</label>
+    <div class="layui-input-block">
+      <input type="text" id = "title" name="title" required  lay-verify="required" placeholder="请输入链接名称" autocomplete="off" class="layui-input">
+    </div>
+  </div>
+  <div class="layui-form-item">
+    <label class="layui-form-label">所属分类</label>
+    <div class="layui-input-block">
+      <select name="fid" lay-verify="required" lay-search>
+        <option value=""></option>
+        <?php foreach ($categorys as $category) {
+          # code...
+        ?>
+        <option value="<?php echo $category['id'] ?>"><?php echo $category['name']; ?></option>
+        <?php } ?>
+      </select>
+    </div>
+  </div>
+  <div class="layui-form-item">
+    <label class="layui-form-label">权重</label>
+    <div class="layui-input-block">
+      <input type="number" name="weight" min = "0" max = "999" value = "0" required  lay-verify="required|number" placeholder="权重越高，排名越靠前，范围为0-999" autocomplete="off" class="layui-input">
+    </div>
+  </div>
+  
+  
+  <div class="layui-form-item">
+    <label class="layui-form-label">是否私有</label>
+    <div class="layui-input-block">
+      <input type="checkbox" name="property" value = "1" lay-skin="switch" lay-text="是|否">
+    </div>
+  </div>
+  
+  <div class="layui-form-item layui-form-text">
+    <label class="layui-form-label">描述</label>
+    <div class="layui-input-block">
+      <textarea name="description" id = "description" placeholder="请输入内容" class="layui-textarea"></textarea>
+    </div>
+  </div>
+  <div class="layui-form-item">
+    <div class="layui-input-block">
+      <button class="layui-btn" lay-submit lay-filter="add_link">添加</button>
+      <!-- <button class="layui-btn" lay-submit lay-filter="get_link_info">识别</button> -->
+      <a href="javascript:;" class="layui-btn" onclick="get_link_info()">识别</a>
+      <button type="reset" class="layui-btn layui-btn-primary">重置</button>
+    </div>
+  </div>
+</form>
+</div>
+    </div>
+<!-- 内容主题区域END -->
+</div>
+  
+<script src = 'https://libs.xiaoz.top/jquery/2.2.4/jquery.min.js'></script>
+<script src = 'https://libs.xiaoz.top/layui/v2.5.4/layui.js'></script>
+<script src="templates/admin/static/embed.js"></script>
+
+</body>
+</html>
