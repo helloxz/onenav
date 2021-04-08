5e8109bc938dbeb0accd78e90cac2472bb0be2af
xiaoz <xiaoz93@outlook.com>
2021-04-08 18:26:34 +0800

    test

---
+++ test.php
@@ -0,0 +1,24 @@
+<?php
+$content = file_get_contents('./bookmarks_2021_4_8.html');
+
+$pattern = "/<A.*<\/A>/i";
+
+preg_match_all($pattern,$content,$arr);
+
+foreach( $arr[0] as $link )
+{
+	//var_dump($link);
+	#$pattern = "/href=\"(.*)\"? ADD_DATE/i";
+	$pattern = "/http.*\"? ADD_DATE/i";
+	preg_match($pattern,$link,$urls);
+	$url = str_replace('" ADD_DATE','',$urls[0]);
+	$pattern = "/>.*<\/a>$/i";
+	preg_match($pattern,$link,$titles);
+	
+	$title = str_replace('>','',$titles[0]);
+	$title = str_replace('</A','',$title);
+	//echo $url;
+	//exit;
+	echo $title.' - '.$url."<br />";
+	//echo 'dsd';
+}
