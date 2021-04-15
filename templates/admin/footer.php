dc617a7d20134af99e059af695ff260ca67f4a82
Maruko <zhxuhu@outlook.com>
2021-04-15 11:24:26 +0800

    Update footer.php

--- templates/admin/footer.php
+++ templates/admin/footer.php
@@ -3,8 +3,8 @@
     © Copyright 2021.Powered by <a href="https://github.com/helloxz/onenav" rel = "nofollow" target="_blank">OneNav</a>.
   </div>
 </div>
-<script src = 'https://libs.xiaoz.top/jquery/2.2.4/jquery.min.js'></script>
-<script src = 'https://libs.xiaoz.top/layui/v2.5.4/layui.js'></script>
+<script src = 'https://cdn.jsdelivr.net/npm/jquery@2.2.4/dist/jquery.min.js'></script>
+<script src = 'https://cdn.jsdelivr.net/gh/sentsin/layui@efb865266164af6e570cc99514f6e830f04c858d/dist/layui.js'></script>
 <script src="templates/admin/static/embed.js?v=<?php echo $version; ?>"></script>
 
 </body>
