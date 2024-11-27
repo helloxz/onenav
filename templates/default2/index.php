34a38d13c1ced99604b72fc5313efa01b74a6474
xiaoz <xiaoz93@outlook.com>
2024-11-27 17:04:47 +0800

    default2

---
+++ templates/default2/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+  <head>
+    <meta charset="utf-8">
+    <meta content="<?php echo $site['keywords']; ?>" name="keywords">
+    <meta content="<?php echo $site['description']; ?>" name="description">
+    <meta content="IE=edge" http-equiv="X-UA-Compatible">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <title>
+      <?php echo $site[ 'title']; ?> - <?php echo $site[ 'subtitle']; ?></title>
+      <script type="module" crossorigin src="/templates/default2/assets/index.js?v=<?php echo $info->version; ?>""></script>
+    </script>
+    <link href="/templates/default2/assets/index.css?v=<?php echo $info->version; ?>"" rel="stylesheet">
+    <?php echo $site['custom_header']; ?>
+    <link rel="manifest" href="/templates/default2/manifest.json" />
+  </head>
+  
+  <body>
+    <div id="app"></div>
+  </body>
+
+</html>
