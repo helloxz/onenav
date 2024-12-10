c6382a67a94187296c7a050dbd62a7e1f62274dc
xiaoz <xiaoz93@outlook.com>
2024-12-10 10:41:35 +0800

    fix bug

--- templates/admin/edit_link_new.php
+++ templates/admin/edit_link_new.php
@@ -22,7 +22,7 @@
     <div class="layui-form-item">
     <label class="layui-form-label">URL</label>
     <div class="layui-input-block">
-      <input type="url" id = "url" name="url" value = "<?php echo $link['url']; ?>" required  lay-verify="required|url" placeholder="请输入有效链接" autocomplete="off" class="layui-input">
+      <input type="url" id = "url" name="url" value = "<?php echo $link['url']; ?>" required  lay-verify="required" placeholder="请输入有效链接" autocomplete="off" class="layui-input">
     </div>
   </div>
 
--- templates/admin/footer.php
+++ templates/admin/footer.php
@@ -1,6 +1,6 @@
 <div class="layui-footer">
     <!-- 底部固定区域 -->
-    © Copyright 2024.Powered by <a href="https://www.onenav.top/" rel = "nofollow" target="_blank">OneNav</a>.
+    © Copyright <?php echo date("Y"); ?>.Powered by <a href="https://www.onenav.top/" rel = "nofollow" target="_blank">OneNav</a>.
   </div>
 </div>
 </body>
