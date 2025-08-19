8e96d82cb59f4a9b9659ceceb3c7fcdc327c7454
xiaoz <xiaoz93@outlook.com>
2025-08-19 09:12:48 +0800

    fix default2

--- templates/default2/index.php
+++ templates/default2/index.php
@@ -6,10 +6,8 @@
     <meta content="<?php echo $site['description']; ?>" name="description">
     <meta content="IE=edge" http-equiv="X-UA-Compatible">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-    <title>
-      <?php echo $site[ 'title']; ?> - <?php echo $site[ 'subtitle']; ?></title>
-      <script type="module" crossorigin src="/templates/<?php echo $template; ?>/assets/index.js?v=<?php echo $info->version; ?>"></script>
-    </script>
+    <title><?php echo $site[ 'title']; ?> - <?php echo $site[ 'subtitle']; ?></title>
+    <script type="module" crossorigin src="/templates/<?php echo $template; ?>/assets/index.js?v=<?php echo $info->version; ?>"></script>
     <link href="/templates/<?php echo $template; ?>/assets/index.css?v=<?php echo $info->version; ?>" rel="stylesheet">
     <?php echo $site['custom_header']; ?>
     <link rel="manifest" href="/templates/<?php echo $template; ?>/manifest.json" />
